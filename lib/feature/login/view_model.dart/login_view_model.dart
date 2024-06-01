21b7344b653b13d98713456519966f29519e5f15
sefadalgic <sefaphoenixx@hotmail.com>
2024-06-01 16:23:35 +0300

    Added Login Screen

---
+++ lib/feature/login/view_model.dart/login_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'login_view_model.g.dart';
+
+class LoginViewModel = _LoginViewModelBase with _$LoginViewModel;
+
+abstract class _LoginViewModelBase with Store {
+  
+}
